5ff3a9c4e84239c82dbf6a19dcb631406363c1a6
Nirmal Fernando <Nirmal@Nirmal-MacBook-Pro.local>
2023-07-04 22:50:42 +1200

    Initial Commit

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+const Contact: React.FC = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Contact</h1>
+    </main>
+  );
+};
+export default Contact;
---
+++ app/hobbies/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Box } from "@chakra-ui/react";
+
+const Hobbies: React.FC = () => {
+  return (
+    <Box as="main">
+      <h1>Hobbies</h1>
+    </Box>
+  );
+};
+export default Hobbies;
