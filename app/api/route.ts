b8d7a6993e3367d0410c9a2d89d6b6bc24a1d638
Frank <frank@downing.nz>
2023-07-14 17:33:01 +1200

    Api request test

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,4 +1,10 @@
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 export async function GET(request: NextRequest) {
   console.log(request)
+  let json_response = {
+    status: 'success',
+    results: request.geo,
+    request,
+  }
+  return NextResponse.json(json_response)
 }
