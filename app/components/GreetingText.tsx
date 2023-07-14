b14f4c387365f29c8afcb103d88f1c45ba710ad8
Frank <frank@downing.nz>
2023-07-14 17:26:42 +1200

    NextRequest

--- app/components/GreetingText.tsx
+++ app/components/GreetingText.tsx
@@ -2,8 +2,6 @@ import { Text, useColorModeValue } from '@chakra-ui/react'
 import { NextRequest, NextResponse } from 'next/server'
 
 const GreetingText = () => {
-  const res = NextResponse.next()
-  console.log(NextRequest.geo)
   return (
     <Text
       ml={2}
